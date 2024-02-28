98e050c5ac0d6cfacff20656f39980ff67f7b71e
z.pirrello <zpirrello@gmail.com>
2024-02-28 12:05:32 -0600

    initial commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'b22cbfb6f1358d11ae46');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'cdb0056876378ec16191');
